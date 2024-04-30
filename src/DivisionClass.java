86ad19df992b23e4021fa6a86ff24a136058535d
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-04-30 13:08:15 -0400

    DivisionClass is updated

--- src/DivisionClass.java
+++ src/DivisionClass.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -7,23 +8,22 @@ public class DivisionClass {
         System.out.println("Enter your string: ");
         String s = sc.nextLine();
 
-        int arr[]=new int[s.length()];
+        ArrayList<Integer> list=new ArrayList<Integer>();
 
         int counter=0;
         for (int i = 0; i < s.length(); i++) {
 
             if (Character.isDigit(s.charAt(i))) {
-
-
-//                System.out.print("Number " + counter + " is: "  + s.charAt(i)+"\n");
-
-                arr[counter]=Character.getNumericValue(s.charAt(i));
-                counter++;
-
-
+                list.add(Character.getNumericValue(s.charAt(i)));
             }
         }
-        System.out.println(Arrays.toString(arr));
+        System.out.println(list);
+        int div=list.get(0);
+
+        for(int i=1;i<=list.size()-1;i++){
+             div=div/(list.get(i));
+        }
+        System.out.println("Your Division result is: "+div);
 
     }
 }
