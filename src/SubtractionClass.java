ba965f561a3c01ac4aca5cee426397ce7b7c20ca
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-04-30 17:05:13 -0400

    commit for other programs added into the repository.

---
+++ src/SubtractionClass.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SubtractionClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your string: ");
+        String s = sc.nextLine();
+
+        ArrayList<Integer> list=new ArrayList<Integer>();
+
+        int counter=0;
+        for (int i = 0; i < s.length(); i++) {
+
+            if (Character.isDigit(s.charAt(i))) {
+                list.add(Character.getNumericValue(s.charAt(i)));
+            }
+        }
+        System.out.println(list);
+        int sub=list.get(0);
+
+        for(int i=1;i<=list.size()-1;i++){
+            sub=sub-(list.get(i));
+        }
+        System.out.println("Your Division result is: "+sub);
+
+    }
+}
